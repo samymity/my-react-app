34242abdc835c3042dee9e93ee1e2fcbe26e3c7d
samymity <samymity@gmail.com>
2026-03-13 15:37:46 -0500

    fix

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
         <header style={{ background: '#d32f2f', color: 'white', padding: '1rem 2rem', width: '100%', boxSizing: 'border-box' }}>
-            <h1 style={{ margin: 0, textAlign: 'center' }}>Brick World 🧱</h1>
+            <h1 style={{ margin: 0, textAlign: 'center' }}>Incident-Managemet-System </h1>
         </header>
     );
 }
